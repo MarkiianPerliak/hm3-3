046e69cdd24b8cb748ac438c6285318736b6248a
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2024-09-21 09:48:58 +0300

    Done

---
+++ index.js
@@ -0,0 +1,36 @@
+// 1 start
+
+const result = 5+5+"5";
+
+console.log(result)
+
+// 1 end
+
+// 2 start
+
+const email = "myemail@gmail.com";
+
+console.log(email.includes("@"))
+
+console.log(email.length)
+
+// 2 end
+
+// 3 start
+
+const mess = "My name is "
+const myName = "Viktor";
+const fullMessage = mess + myName;
+console.log(fullMessage)
+
+// 3 end
+
+// 4 start
+
+const userName = "Олександро";
+
+const payment = "300";
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
+
+// 4 end
